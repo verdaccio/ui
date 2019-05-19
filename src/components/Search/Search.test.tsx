0b7f4fbc28a0cf02469eabf7ce9b287fcbfb4ee9
Priscila Oliveira <priscilawebdev@gmail.com>
2019-05-19 22:53:28 +0200

    fix: fixed search test

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -3,7 +3,6 @@ import { mount, shallow } from 'enzyme';
 import { BrowserRouter } from 'react-router-dom';
 
 import Search from './Search';
-import AutoComplete from '../AutoComplete';
 
 const SEARCH_FILE_PATH = './Search';
 const API_FILE_PATH = '../../utils/api';
@@ -16,9 +15,10 @@ const event = {
 window.location.assign = jest.fn();
 
 describe('<Search /> component test', () => {
+  let routerWrapper;
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(
+    routerWrapper = mount(
       <BrowserRouter>
         <Search />
       </BrowserRouter>
@@ -26,129 +26,10 @@ describe('<Search /> component test', () => {
   });
 
   test('should load the component in default state', () => {
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(routerWrapper.html()).toMatchSnapshot();
   });
 
-  // test('onBlur: should cancel all search requests', async () => {
-  //   const { onBlur, requestList } = wrapper.instance();
-  //   const spy = jest.spyOn(wrapper.instance(), 'cancelAllSearchRequests');
-
-  //   const request = {
-  //     abort: jest.fn(),
-  //   };
-  //   // adds a request for AbortController
-  //   wrapper.instance().requestList = [request];
-
-  //   onBlur(event);
-
-  //   expect(request.abort).toHaveBeenCalled();
-  //   expect(event.stopPropagation).toHaveBeenCalled();
-  //   expect(wrapper.state('error')).toBeFalsy();
-  //   expect(wrapper.state('loaded')).toBeFalsy();
-  //   expect(wrapper.state('loading')).toBeFalsy();
-  //   expect(spy).toHaveBeenCalled();
-  //   expect(requestList).toEqual([]);
-  // });
-
-  // test('handleSearch: when user type package name in search component and set loading to true', () => {
-  //   const { handleSearch } = wrapper.dive().instance();
-  //   const newValue = 'verdaccio';
-
-  //   handleSearch(event, { newValue, method: 'type' });
-
-  //   expect(event.stopPropagation).toHaveBeenCalled();
-  //   expect(wrapper.state('error')).toBeFalsy();
-  //   expect(wrapper.state('loaded')).toBeFalsy();
-  //   expect(wrapper.state('loading')).toBeTruthy();
-  //   expect(wrapper.state('search')).toEqual(newValue);
-  // });
-
-  //   test.only('handleSearch: cancel all search requests when there is no value in search component with type method', () => {
-  //     const { handleSearch, requestList } = wrapper.instance();
-  //     const spy = jest.spyOn(wrapper.instance(), 'cancelAllSearchRequests');
-  //     const newValue = '';
-
-  //     handleSearch(event, { newValue, method: 'type' });
-
-  //     expect(event.stopPropagation).toHaveBeenCalled();
-  //     expect(wrapper.state('error')).toBeFalsy();
-  //     expect(wrapper.state('loaded')).toBeFalsy();
-  //     expect(wrapper.state('loading')).toBeTruthy();
-  //     expect(wrapper.state('search')).toEqual(newValue);
-  //     expect(spy).toHaveBeenCalled();
-  //     expect(requestList).toEqual([]);
-  //   });
-
-  //   test('handleSearch: when method is not type method', () => {
-  //     const { handleSearch } = wrapper.instance();
-  //     const newValue = '';
-
-  //     handleSearch(event, { newValue, method: 'click' });
-
-  //     expect(event.stopPropagation).toHaveBeenCalled();
-  //     expect(wrapper.state('error')).toBeFalsy();
-  //     expect(wrapper.state('loaded')).toBeFalsy();
-  //     expect(wrapper.state('loading')).toBeFalsy();
-  //     expect(wrapper.state('search')).toEqual(newValue);
-  //   });
-
-  //   test('handlePackagesClearRequested: should clear suggestions', () => {
-  //     const { handlePackagesClearRequested } = wrapper.instance();
-
-  //     handlePackagesClearRequested();
-
-  //     expect(wrapper.state('suggestions')).toEqual([]);
-  //   });
-  // });
-
-  // describe('<Search /> component: mocks specific tests ', () => {
-  //   beforeEach(() => {
-  //     jest.resetModules();
-  //     jest.doMock('lodash/debounce', () => {
-  //       return function debounceMock(fn, delay) {
-  //         return fn;
-  //       };
-  //     });
-  //   });
-
-  test('handleFetchPackages: should load the packages from API', async () => {
-    const apiResponse = [{ name: 'verdaccio' }, { name: 'verdaccio-htpasswd' }];
-    const suggestions = [{ name: 'verdaccio' }, { name: 'verdaccio-htpasswd' }];
-
-    jest.doMock(API_FILE_PATH, () => ({
-      request(url) {
-        expect(url).toEqual('search/verdaccio');
-        return Promise.resolve(apiResponse);
-      },
-    }));
-
-    const Search = require(SEARCH_FILE_PATH).Search;
-
-    const routerWrapper = shallow(
-      <BrowserRouter>
-        <Search />
-      </BrowserRouter>
-    );
-
-    wrapper = routerWrapper.find(Search).dive();
-    // wrapper.setState({ search: 'verdaccio' });
-    const { handleFetchPackages } = wrapper.instance();
-
-    await handleFetchPackages({ value: 'verdaccio' });
-
-    expect(wrapper.state('suggestions')).toEqual(suggestions);
-    expect(wrapper.state('error')).toBeTruthy();
-    expect(wrapper.state('loaded')).toBeTruthy();
-    expect(wrapper.state('loading')).toBeFalsy();
-  });
-
-  test('handleFetchPackages: when browser cancel a request', async () => {
-    const apiResponse = { name: 'AbortError' };
-
-    jest.doMock(API_FILE_PATH, () => ({ request: jest.fn(() => Promise.reject(apiResponse)) }));
-
-    const Search = require(SEARCH_FILE_PATH).Search;
-
+  test('onBlur: should cancel all search requests', async () => {
     const routerWrapper = shallow(
       <BrowserRouter>
         <Search />
@@ -157,26 +38,28 @@ describe('<Search /> component test', () => {
 
     wrapper = routerWrapper.find(Search).dive();
     wrapper.setState({ search: 'verdaccio' });
-    const { handleFetchPackages } = wrapper.instance();
+    const { onBlur, requestList, cancelAllSearchRequests } = wrapper.instance();
 
-    await handleFetchPackages({ value: 'verdaccio' });
+    const request = {
+      abort: jest.fn(),
+    };
+    // adds a request for AbortController
+    wrapper.instance().requestList = [request];
 
-    expect(wrapper.state('error')).toBeTruthy();
+    onBlur(event);
+
+    expect(request.abort).toHaveBeenCalled();
+    expect(event.stopPropagation).toHaveBeenCalled();
+    expect(wrapper.state('error')).toBeFalsy();
     expect(wrapper.state('loaded')).toBeFalsy();
     expect(wrapper.state('loading')).toBeFalsy();
+    expect(cancelAllSearchRequests).toHaveBeenCalled();
+    expect(requestList).toEqual([]);
   });
 
-  test('handleFetchPackages: when API server failed request', async () => {
-    const apiResponse = { name: 'BAD_REQUEST' };
-
-    jest.doMock(API_FILE_PATH, () => ({
-      request(url) {
-        expect(url).toEqual('search/verdaccio');
-        return Promise.reject(apiResponse);
-      },
-    }));
-
+  test('handleSearch: when user type package name in search component and set loading to true', () => {
     const Search = require(SEARCH_FILE_PATH).Search;
+
     const routerWrapper = shallow(
       <BrowserRouter>
         <Search />
@@ -184,40 +67,204 @@ describe('<Search /> component test', () => {
     );
 
     wrapper = routerWrapper.find(Search).dive();
-    wrapper.setState({ search: 'verdaccio' });
-    const { handleFetchPackages } = wrapper.instance();
 
-    await handleFetchPackages({ value: 'verdaccio' });
+    const { handleSearch } = wrapper.instance();
+    const newValue = 'verdaccio';
 
-    expect(wrapper.state('error')).toBeTruthy();
+    handleSearch(event, { newValue, method: 'type' });
+
+    expect(event.stopPropagation).toHaveBeenCalled();
+    expect(wrapper.state('error')).toBeFalsy();
     expect(wrapper.state('loaded')).toBeFalsy();
-    expect(wrapper.state('loading')).toBeFalsy();
+    expect(wrapper.state('loading')).toBeTruthy();
+    expect(wrapper.state('search')).toEqual(newValue);
   });
 
-  test('handleClickSearch: should change the window location on click or return key', () => {
-    const getDetailPageURL = jest.fn(() => 'detail/page/url');
-    jest.doMock(URL_FILE_PATH, () => ({ getDetailPageURL }));
-
-    const suggestionValue = [];
+  test.only('handleSearch: cancel all search requests when there is no value in search component with type method', () => {
     const Search = require(SEARCH_FILE_PATH).Search;
-    const pushHandler = jest.fn();
+
     const routerWrapper = shallow(
       <BrowserRouter>
-        <Search history={{ push: pushHandler }} />
+        <Search />
       </BrowserRouter>
     );
 
     wrapper = routerWrapper.find(Search).dive();
-    const { handleClickSearch } = wrapper.instance();
 
-    // click
-    handleClickSearch(event, { suggestionValue, method: 'click' });
-    expect(event.stopPropagation).toHaveBeenCalled();
-    expect(pushHandler).toHaveBeenCalledTimes(1);
+    const { handleSearch, requestList } = wrapper.instance();
+    const spy = jest.spyOn(wrapper.instance(), 'cancelAllSearchRequests');
+    const newValue = '';
 
-    // return key
-    handleClickSearch(event, { suggestionValue, method: 'enter' });
+    handleSearch(event, { newValue, method: 'type' });
+
     expect(event.stopPropagation).toHaveBeenCalled();
-    expect(pushHandler).toHaveBeenCalledTimes(2);
+    expect(wrapper.state('error')).toBeFalsy();
+    expect(wrapper.state('loaded')).toBeFalsy();
+    expect(wrapper.state('loading')).toBeTruthy();
+    expect(wrapper.state('search')).toEqual(newValue);
+    expect(spy).toHaveBeenCalled();
+    expect(requestList).toEqual([]);
+  });
+
+  test('handleSearch: when method is not type method', () => {
+    const Search = require(SEARCH_FILE_PATH).Search;
+
+    const routerWrapper = shallow(
+      <BrowserRouter>
+        <Search />
+      </BrowserRouter>
+    );
+
+    wrapper = routerWrapper.find(Search).dive();
+
+    const { handleSearch } = wrapper.instance();
+    const newValue = '';
+
+    handleSearch(event, { newValue, method: 'click' });
+
+    expect(event.stopPropagation).toHaveBeenCalled();
+    expect(wrapper.state('error')).toBeFalsy();
+    expect(wrapper.state('loaded')).toBeFalsy();
+    expect(wrapper.state('loading')).toBeFalsy();
+    expect(wrapper.state('search')).toEqual(newValue);
+  });
+
+  test('handlePackagesClearRequested: should clear suggestions', () => {
+    const Search = require(SEARCH_FILE_PATH).Search;
+
+    const routerWrapper = shallow(
+      <BrowserRouter>
+        <Search />
+      </BrowserRouter>
+    );
+
+    wrapper = routerWrapper.find(Search).dive();
+
+    const { handlePackagesClearRequested } = wrapper.instance();
+
+    handlePackagesClearRequested();
+
+    expect(wrapper.state('suggestions')).toEqual([]);
+  });
+
+  describe('<Search /> component: mocks specific tests ', () => {
+    beforeEach(() => {
+      jest.resetModules();
+      jest.doMock('lodash/debounce', () => {
+        return function debounceMock(fn, delay) {
+          return fn;
+        };
+      });
+    });
+
+    test('handleFetchPackages: should load the packages from API', async () => {
+      const apiResponse = [{ name: 'verdaccio' }, { name: 'verdaccio-htpasswd' }];
+      const suggestions = [{ name: 'verdaccio' }, { name: 'verdaccio-htpasswd' }];
+
+      jest.doMock(API_FILE_PATH, () => ({
+        request(url) {
+          expect(url).toEqual('search/verdaccio');
+          return Promise.resolve(apiResponse);
+        },
+      }));
+
+      const Search = require(SEARCH_FILE_PATH).Search;
+
+      const routerWrapper = shallow(
+        <BrowserRouter>
+          <Search />
+        </BrowserRouter>
+      );
+
+      wrapper = routerWrapper.find(Search).dive();
+      wrapper.setState({ search: 'verdaccio' });
+      const { handleFetchPackages } = wrapper.instance();
+
+      await handleFetchPackages({ value: 'verdaccio' });
+
+      expect(wrapper.state('suggestions')).toEqual(suggestions);
+      expect(wrapper.state('error')).toBeTruthy();
+      expect(wrapper.state('loaded')).toBeTruthy();
+      expect(wrapper.state('loading')).toBeFalsy();
+    });
+
+    test('handleFetchPackages: when browser cancel a request', async () => {
+      const apiResponse = { name: 'AbortError' };
+
+      jest.doMock(API_FILE_PATH, () => ({ request: jest.fn(() => Promise.reject(apiResponse)) }));
+
+      const Search = require(SEARCH_FILE_PATH).Search;
+
+      const routerWrapper = shallow(
+        <BrowserRouter>
+          <Search />
+        </BrowserRouter>
+      );
+
+      wrapper = routerWrapper.find(Search).dive();
+      wrapper.setState({ search: 'verdaccio' });
+      const { handleFetchPackages } = wrapper.instance();
+
+      await handleFetchPackages({ value: 'verdaccio' });
+
+      expect(wrapper.state('error')).toBeTruthy();
+      expect(wrapper.state('loaded')).toBeFalsy();
+      expect(wrapper.state('loading')).toBeFalsy();
+    });
+
+    test('handleFetchPackages: when API server failed request', async () => {
+      const apiResponse = { name: 'BAD_REQUEST' };
+
+      jest.doMock(API_FILE_PATH, () => ({
+        request(url) {
+          expect(url).toEqual('search/verdaccio');
+          return Promise.reject(apiResponse);
+        },
+      }));
+
+      const Search = require(SEARCH_FILE_PATH).Search;
+      const routerWrapper = shallow(
+        <BrowserRouter>
+          <Search />
+        </BrowserRouter>
+      );
+
+      wrapper = routerWrapper.find(Search).dive();
+      wrapper.setState({ search: 'verdaccio' });
+      const { handleFetchPackages } = wrapper.instance();
+
+      await handleFetchPackages({ value: 'verdaccio' });
+
+      expect(wrapper.state('error')).toBeTruthy();
+      expect(wrapper.state('loaded')).toBeFalsy();
+      expect(wrapper.state('loading')).toBeFalsy();
+    });
+
+    test('handleClickSearch: should change the window location on click or return key', () => {
+      const getDetailPageURL = jest.fn(() => 'detail/page/url');
+      jest.doMock(URL_FILE_PATH, () => ({ getDetailPageURL }));
+
+      const suggestionValue = [];
+      const Search = require(SEARCH_FILE_PATH).Search;
+      const pushHandler = jest.fn();
+      const routerWrapper = shallow(
+        <BrowserRouter>
+          <Search history={{ push: pushHandler }} />
+        </BrowserRouter>
+      );
+
+      wrapper = routerWrapper.find(Search).dive();
+      const { handleClickSearch } = wrapper.instance();
+
+      // click
+      handleClickSearch(event, { suggestionValue, method: 'click' });
+      expect(event.stopPropagation).toHaveBeenCalled();
+      expect(pushHandler).toHaveBeenCalledTimes(1);
+
+      // return key
+      handleClickSearch(event, { suggestionValue, method: 'enter' });
+      expect(event.stopPropagation).toHaveBeenCalled();
+      expect(pushHandler).toHaveBeenCalledTimes(2);
+    });
   });
 });
