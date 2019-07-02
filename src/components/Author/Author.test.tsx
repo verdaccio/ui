531e7579a48d35dfd48df4c2ae9943470fc78648
Ayush Sharma <ayush.aceit@gmail.com>
2019-07-02 21:37:36 +0200

    chore: adds unit test for <Author/> component

--- src/components/Author/Author.test.tsx
+++ src/components/Author/Author.test.tsx
@@ -1,10 +1,75 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Author from './Author';
-
 describe('<Author /> component', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
   test('should render the component in default state', () => {
+    const packageMeta = {
+      latest: {
+        name: 'verdaccio',
+        version: '4.0.0',
+        author: {
+          name: 'verdaccio user',
+          email: 'verdaccio.user@verdaccio.org',
+          url: '',
+          avatar: 'https://www.gravatar.com/avatar/000000',
+        },
+      },
+    };
+
+    jest.doMock('../../pages/version/Version', () => ({
+      DetailContextConsumer: component => {
+        return component.children({ packageMeta });
+      },
+    }));
+
+    const Author = require('./Author').default;
+    const wrapper = shallow(<Author />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('should render the component when there is no author information available', () => {
+    const packageMeta = {
+      latest: {
+        name: 'verdaccio',
+        version: '4.0.0',
+      },
+    };
+
+    jest.doMock('../../pages/version/Version', () => ({
+      DetailContextConsumer: component => {
+        return component.children({ packageMeta });
+      },
+    }));
+
+    const Author = require('./Author').default;
+    const wrapper = shallow(<Author />);
+    expect(wrapper.html()).toEqual('');
+  });
+
+  test('should render the component when there is no author email', () => {
+    const packageMeta = {
+      latest: {
+        name: 'verdaccio',
+        version: '4.0.0',
+        author: {
+          name: 'verdaccio user',
+          url: '',
+          avatar: 'https://www.gravatar.com/avatar/000000',
+        },
+      },
+    };
+
+    jest.doMock('../../pages/version/Version', () => ({
+      DetailContextConsumer: component => {
+        return component.children({ packageMeta });
+      },
+    }));
+
+    const Author = require('./Author').default;
     const wrapper = shallow(<Author />);
     expect(wrapper.html()).toMatchSnapshot();
   });
