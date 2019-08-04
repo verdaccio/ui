1089686dadb649f33a4921c97ca5ba5930bb3403
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-08-04 22:02:37 +0200

    build: add helper :+1:
    thanks daniel
    
    Co-Authored-By: Daniel Ruf <danielruf@users.noreply.github.com>

--- codecept.multiple.conf.js
+++ codecept.multiple.conf.js
@@ -9,6 +9,23 @@ exports.config = {
   include: {
     I: './test/acceptance/steps_file.js',
   },
+  helpers: {
+    WebDriver: {
+      url: 'hub.browserstack.com',
+      browser: 'Chrome',
+      user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+      key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
+      capabilities: {
+        browserName: 'chrome',
+        platformName: 'windows',
+        // only set this if you're using BrowserStackLocal to test a local domain
+        'browserstack.local': true,
+
+        // set this option to tell browserstack to provide addition debugging info
+        // "browserstack.debug": true,
+      },
+    },
+  },
   smartWait: 30000,
   bootstrapAll: './codeceptjs/browser_stack_local.bootstrap.js',
   teardownAll: './codeceptjs/browser_stack_local.teardown.js',
