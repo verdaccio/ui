3874954491e89a91641aa6ac503d08af8b2bd274
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-09-01 13:00:16 +0200

    chore: remove imports

--- src/pages/Version/Version.tsx
+++ src/pages/Version/Version.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { render, cleanup } from '@testing-library/react';
 
 import { callDetailPage, callReadme } from '../../utils/calls';
 import { buildScopePackage } from '../../utils/package';
