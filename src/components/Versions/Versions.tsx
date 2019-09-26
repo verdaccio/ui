0b34b16bab877bbc1e5a1273c1b326a6b425efa5
Priscila Oliveira <priscilawebdev@gmail.com>
2019-09-26 19:30:24 +0200

    refactor: replaced classes by func comp

--- src/components/Versions/Versions.tsx
+++ src/components/Versions/Versions.tsx
@@ -1,90 +1,48 @@
-import React, { ReactElement } from 'react';
-import List from '@material-ui/core/List';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
-import ListItem from '@material-ui/core/ListItem';
+import React from 'react';
 
 import { DetailContextConsumer } from '../../pages/Version';
-import { formatDateDistance } from '../../utils/package';
-import { DIST_TAGS } from '../../../lib/constants';
 
-import { Heading, Spacer, ListItemText } from './styles';
+import { Heading } from './styles';
+
+import VersionsTagList from './VersionsTagList';
+import VersionsHistoryList from './VersionsHistoryList';
+
+import { DIST_TAGS } from '../../../lib/constants';
 
 export const NOT_AVAILABLE = 'Not available';
 export const LABEL_CURRENT_TAGS = 'Current Tags';
 export const LABEL_VERSION_HISTORY = 'Version History';
 
-class Versions extends React.PureComponent {
-  public render(): ReactElement<HTMLDivElement> {
-    return (
-      <DetailContextConsumer>
-        {context => {
-          const { packageMeta, packageName } = context;
+const Versions: React.FC = () => (
+  <DetailContextConsumer>
+    {context => {
+      const { packageMeta, packageName } = context;
 
-          if (!packageMeta) {
-            return null;
-          }
+      if (!packageMeta) {
+        return null;
+      }
 
-          return this.renderContent(packageMeta, packageName);
-        }}
-      </DetailContextConsumer>
-    );
-  }
+      // @ts-ignore - Property 'dist-tags' does not exist on type 'PackageMetaInterface'
+      const { versions, time, [DIST_TAGS]: distTags } = packageMeta;
 
-  public renderPackageList = (packages: {}, timeMap: Record<string, {}>, packageName): ReactElement<HTMLDivElement> => {
-    return (
-      <List dense={true}>
-        {Object.keys(packages)
-          .reverse()
-          .map(version => (
-            <ListItem className="version-item" key={version}>
-              <Link component={RouterLink} to={`/-/web/detail/${packageName}/v/${version}`}>
-                <ListItemText>{version}</ListItemText>
-              </Link>
-              <Spacer />
-              <ListItemText>{timeMap[version] ? `${formatDateDistance(timeMap[version])} ago` : NOT_AVAILABLE}</ListItemText>
-            </ListItem>
-          ))}
-      </List>
-    );
-  };
-
-  public renderTagList = (packages: {}): ReactElement<HTMLDivElement> => {
-    return (
-      <List dense={true}>
-        {Object.keys(packages)
-          .reverse()
-          .map(tag => (
-            <ListItem className="version-item" key={tag}>
-              <ListItemText>{tag}</ListItemText>
-              <Spacer />
-              <ListItemText>{packages[tag]}</ListItemText>
-            </ListItem>
-          ))}
-      </List>
-    );
-  };
-
-  public renderContent(packageMeta, packageName): ReactElement<HTMLDivElement> {
-    const { versions = {}, time: timeMap = {}, [DIST_TAGS]: distTags = {} } = packageMeta;
-
-    return (
-      <>
-        {distTags && (
-          <>
-            <Heading variant="subtitle1">{LABEL_CURRENT_TAGS}</Heading>
-            {this.renderTagList(distTags)}
-          </>
-        )}
-        {versions && (
-          <>
-            <Heading variant="subtitle1">{LABEL_VERSION_HISTORY}</Heading>
-            {this.renderPackageList(versions, timeMap, packageName)}
-          </>
-        )}
-      </>
-    );
-  }
-}
+      return (
+        <>
+          {distTags && (
+            <>
+              <Heading variant="subtitle1">{LABEL_CURRENT_TAGS}</Heading>
+              <VersionsTagList tags={distTags} />
+            </>
+          )}
+          {versions && packageName && (
+            <>
+              <Heading variant="subtitle1">{LABEL_VERSION_HISTORY}</Heading>
+              <VersionsHistoryList packageName={packageName} time={time} versions={versions} />
+            </>
+          )}
+        </>
+      );
+    }}
+  </DetailContextConsumer>
+);
 
 export default Versions;
--- types/packageMeta.ts
+++ types/packageMeta.ts
@@ -1,4 +1,7 @@
 export interface PackageMetaInterface {
+  versions: Versions;
+  distTags: DistTags;
+  time: Time;
   latest: {
     name: string;
     dist: {
@@ -13,4 +16,34 @@ export interface PackageMetaInterface {
 interface LicenseInterface {
   type: string;
   url: string;
+}
+
+export interface DistTags {
+  [key: string]: string;
+}
+
+export interface Time {
+  [key: string]: string;
+}
+
+export interface Versions {
+  author: string | Author;
+  maintainers: Maintainer[];
+  name: string;
+  description: string;
+  license: string;
+  main: string;
+  version: string;
+  keywords: string[];
+}
+
+interface Author {
+  name: string;
+  email: string;
+  url?: string;
+}
+
+interface Maintainer {
+  email: string;
+  name: string;
 }
