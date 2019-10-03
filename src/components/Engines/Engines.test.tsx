852f6eeb22be4f9980da6cca6e367b65ca3d5ba7
Antoine Chalifour <achalifour@octo.com>
2019-10-03 13:23:11 +0200

    fix: improve jest mock typings

--- src/components/Engines/Engines.test.tsx
+++ src/components/Engines/Engines.test.tsx
@@ -5,7 +5,7 @@ import Engine from './Engines';
 jest.mock('./img/node.png', () => '');
 jest.mock('../Install/img/npm.svg', () => '');
 
-const mockPackageMeta = jest.fn(() => ({
+const mockPackageMeta: jest.Mock = jest.fn(() => ({
   latest: {
     homepage: 'https://verdaccio.tld',
     bugs: {
@@ -38,7 +38,6 @@ describe('<Engines /> component', () => {
       },
     };
 
-    // @ts-ignore
     mockPackageMeta.mockImplementation(() => packageMeta);
 
     const wrapper = mount(<Engine />);
@@ -50,7 +49,6 @@ describe('<Engines /> component', () => {
       latest: {},
     };
 
-    // @ts-ignore
     mockPackageMeta.mockImplementation(() => packageMeta);
 
     const wrapper = mount(<Engine />);
@@ -64,7 +62,6 @@ describe('<Engines /> component', () => {
       },
     };
 
-    // @ts-ignore
     mockPackageMeta.mockImplementation(() => packageMeta);
 
     const wrapper = mount(<Engine />);
