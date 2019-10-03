705530c58c724ca7e419c1c018b2f9e4bc6060a2
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-03 15:32:13 +0200

    refactor: convert class to func comp

--- src/components/Install/Install.test.tsx
+++ src/components/Install/Install.test.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
+
+import { DetailContext, DetailContextProps } from '../../pages/Version';
+import data from '../Versions/__partials__/data.json';
 
 import Install from './Install';
 
-describe('<Install /> component', () => {
-  test('should render the component in default state', () => {
-    const wrapper = mount(<Install />);
-    expect(wrapper.html()).toMatchSnapshot();
+const detailContextValue: Partial<DetailContextProps> = {
+  packageName: 'foo',
+  packageMeta: data,
+};
+
+const ComponentToBeRendered: React.FC = () => (
+  <DetailContext.Provider value={detailContextValue}>
+    <Install />
+  </DetailContext.Provider>
+);
+
+/* eslint-disable react/jsx-no-bind*/
+describe('<Install />', () => {
+  test('renders correctly', () => {
+    const { container } = render(<ComponentToBeRendered />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('should have 3 children', () => {
+    const { getByTestId } = render(<ComponentToBeRendered />);
+    const installListItems = getByTestId('installList');
+    // installitems + subHeader = 4
+    expect(installListItems.children.length).toBe(4);
+  });
+
+  test('should have the element NPM', () => {
+    const { getByTestId } = render(<ComponentToBeRendered />);
+    expect(getByTestId('installListItem-npm')).toBeTruthy();
+  });
+
+  test('should have the element YARN', () => {
+    const { getByTestId } = render(<ComponentToBeRendered />);
+    expect(getByTestId('installListItem-yarn')).toBeTruthy();
+  });
+
+  test('should have the element PNPM', () => {
+    const { getByTestId } = render(<ComponentToBeRendered />);
+    expect(getByTestId('installListItem-pnpm')).toBeTruthy();
   });
 });
