a8deeb9b9d2cb15e61474c904ec4d65f64cc8b1e
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-12 21:41:42 +0200

    fix: typography Component - Introduced ForwardRef (#179)
    * refactor: introduced forwardref
    
    * refacttor: updated ref's
    
    * fix: fixed func's name
    
    * fix: fixed snapshots
    
    * fix: updated snap

--- src/components/Engines/styles.ts
+++ src/components/Engines/styles.ts
@@ -1,14 +1,12 @@
 import styled from 'react-emotion';
-import Typography from '@material-ui/core/Typography';
 
 import { fontWeight } from '../../utils/styles/sizes';
 import ListItem from '../../muiComponents/ListItem';
+import Text from '../../muiComponents/Text';
 
-export const Heading = styled(Typography)({
-  '&&': {
-    fontWeight: fontWeight.bold,
-    textTransform: 'capitalize',
-  },
+export const StyledText = styled(Text)({
+  fontWeight: fontWeight.bold,
+  textTransform: 'capitalize',
 });
 
 export const EngineListItem = styled(ListItem)({
