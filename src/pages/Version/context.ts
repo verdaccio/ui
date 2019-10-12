ccc4af4dd9638e30bcc04b69f04c0ee4245a5d5c
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-12 20:48:45 +0200

    refactor: rollback context

--- src/pages/Version/context.ts
+++ src/pages/Version/context.ts
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, Consumer, Provider } from 'react';
 
 import { PackageMetaInterface } from '../../../types/packageMeta';
 export interface DetailContextProps {
@@ -11,6 +11,16 @@ export interface DetailContextProps {
   hasNotBeenFound: boolean;
 }
 
+export interface VersionPageConsumerProps {
+  packageMeta: PackageMetaInterface;
+  readMe: string;
+  packageName: string;
+  packageVersion?: string;
+  // FIXME: looking for the appropiated type here
+  enableLoading: any;
+}
+
 export const DetailContext = createContext<Partial<DetailContextProps>>({});
-export const DetailContextProvider = DetailContext.Provider;
-export const DetailContextConsumer = DetailContext.Consumer;
+
+export const DetailContextProvider: Provider<Partial<VersionPageConsumerProps>> = DetailContext.Provider;
+export const DetailContextConsumer: Consumer<Partial<VersionPageConsumerProps>> = DetailContext.Consumer;
