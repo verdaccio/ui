46dd80c04b1797c461d9c45249b10e5cccad26d5
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-10-13 07:18:05 +0200

    build: add e2e testing scripts

---
+++ test/e2e/setup.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const { green } = require('kleur');
+const puppeteer = require('puppeteer');
+const mkdirp = require('mkdirp');
+
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
+
+module.exports = async function() {
+  console.log(green('Setup Puppeteer'));
+  const browser = await puppeteer.launch({ headless: true, /* slowMo: 300 */ args: ['--no-sandbox'] });
+  global.__BROWSER__ = browser;
+  mkdirp.sync(DIR);
+  fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
+};
