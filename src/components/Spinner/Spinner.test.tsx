ff791a35f7e820d43ad519b7a2deb18576d16cd6
hugoazevedosoares <hugo.soares@outlook.com>
2019-10-17 06:52:11 -0300

    chore(#207): added more descriptive tests

--- src/components/Spinner/Spinner.test.tsx
+++ src/components/Spinner/Spinner.test.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Spinner from './Spinner';
+import { Circular, Wrapper } from './styles';
 
 describe('<Spinner /> component', () => {
   test('should render the component in default state', () => {
     const wrapper = shallow(<Spinner />);
+    const wrapperComponent = wrapper.find(Wrapper);
+    const circular = wrapper.find(Circular);
+
+    expect(circular.props().size).toBe(50);
+    expect(wrapperComponent.props().centered).toBe(false);
+  });
+
+  test('should render the component in custom state', () => {
+    const wrapper = shallow(<Spinner centered={true} size={10} />);
+    const wrapperComponent = wrapper.find(Wrapper);
+    const circular = wrapper.find(Circular);
+
+    expect(circular.props().size).toBe(10);
+    expect(wrapperComponent.props().centered).toBe(true);
+
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
