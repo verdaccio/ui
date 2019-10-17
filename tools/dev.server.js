89c0d0a4d2de2c2905ce67c89247fdd1c2f55455
Priscila Oliveira <priscilawebdev@gmail.com>
2019-10-17 13:09:47 +0200

    fix: rollback port

--- tools/dev.server.js
+++ tools/dev.server.js
@@ -37,7 +37,7 @@ new WebpackDevServer(compiler, {
   proxy: [
     {
       context: ['/-/verdaccio/**', '**/*.tgz'],
-      target: 'http://localhost:4872',
+      target: 'http://localhost:8080',
     },
   ],
 }).listen(4872, 'localhost', function(err) {
