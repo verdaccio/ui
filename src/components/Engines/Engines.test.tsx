a0169f594e1acb4f7ca00a5050b23d980ebcc100
Andrew Hughson <andrewhughson@bulb.co.uk>
2019-10-29 21:47:51 +0000

    remove  from engines tests

--- src/components/Engines/Engines.test.tsx
+++ src/components/Engines/Engines.test.tsx
@@ -2,38 +2,38 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { DetailContext } from '../../pages/Version';
+import { PackageMetaInterface } from '../../../types/packageMeta';
 
 import Engine from './Engines';
 
 jest.mock('./img/node.png', () => '');
 jest.mock('../Install/img/npm.svg', () => '');
 
-const withEngineComponent = (packageMeta: React.ContextType<typeof DetailContext>['packageMeta']): JSX.Element => (
+const withEngineComponent = (packageMeta: PackageMetaInterface): JSX.Element => (
   <DetailContext.Provider value={{ packageMeta }}>
     <Engine />
   </DetailContext.Provider>
 );
 
+const mockPackageMeta = (): PackageMetaInterface => ({
+  latest: {
+    name: 'verdaccio',
+    version: '0.0.0',
+    dist: {
+      fileCount: 1,
+      unpackedSize: 1,
+    },
+  },
+  _uplinks: {},
+});
+
 describe('<Engines /> component', () => {
   test('should render the component in default state', () => {
-    const packageMeta = {
-      latest: {
-        name: 'verdaccio',
-        version: '0.0.0',
-        homepage: 'https://verdaccio.tld',
-        bugs: {
-          url: 'https://verdaccio.tld/bugs',
-        },
-        dist: {
-          fileCount: 1,
-          unpackedSize: 1,
-        },
-        engines: {
-          node: '>= 0.1.98',
-          npm: '>3',
-        },
-      },
-      _uplinks: {},
+    const packageMeta = mockPackageMeta();
+
+    packageMeta.latest.engines = {
+      node: '>= 0.1.98',
+      npm: '>3',
     };
 
     const wrapper = mount(withEngineComponent(packageMeta));
@@ -41,24 +41,20 @@ describe('<Engines /> component', () => {
   });
 
   test('should render the component when there is no engine key in package meta', () => {
-    const packageMeta = {
-      latest: {},
-    };
+    const packageMeta = mockPackageMeta();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const wrapper = mount(withEngineComponent(packageMeta as any));
+    delete packageMeta.latest.engines;
+
+    const wrapper = mount(withEngineComponent(packageMeta));
     expect(wrapper.html()).toBeNull();
   });
 
   test('should render the component when there is no keys in engine in package meta', () => {
-    const packageMeta = {
-      latest: {
-        engines: {},
-      },
-    };
+    const packageMeta = mockPackageMeta();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const wrapper = mount(withEngineComponent(packageMeta as any));
+    packageMeta.latest.engines = {};
+
+    const wrapper = mount(withEngineComponent(packageMeta));
     expect(wrapper.html()).toBeNull();
   });
 });
