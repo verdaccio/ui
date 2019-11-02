b74ca2285e818ac8fae8d0923c1e328e8ceabbaf
Alfonso Austin <22054505+agar23@users.noreply.github.com>
2019-11-02 12:53:21 -0400

    fix: refactor/116 RegistryInfoContent is converted to functional component (#229)
    * refactor:116[PackageList] component is converted to functional
    
    * Refactor:#116 - Registry info content is converted to functional component
    
    * refactor/116 - fix lint error
    
    * refactor:116 - more lint errors
    
    * refactor/116 - lint error
    
    * refactor:116 - remove snapshot
    
    * refactor: address code review comments #116
    
    * refactor: fix lint error
    
    * refactor: code review changes
    
    * refactor add missed file
    
    * refactor: lint error
    
    * refactor: lint
    
    * refactor: lint
    
    * refactor: fix lint error

--- src/components/PackageList/PackageList.tsx
+++ src/components/PackageList/PackageList.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, ReactElement } from 'react';
+import React, { Fragment, ReactNode } from 'react';
 
 import Package from '../Package';
 import Help from '../Help';
@@ -12,22 +12,23 @@ interface Props {
   packages: PackageInterface[];
 }
 
-export const PackageList: React.FC<Props> = props => {
-  const renderPackages: () => ReactElement<HTMLElement>[] = () => {
-    return props.packages.map((pkg, i) => {
-      const { name, version, description, time, keywords, dist, homepage, bugs, author } = pkg;
+export const PackageList: React.FC<Props> = ({ packages }) => {
+  const renderPackages: () => ReactNode[] = () => {
+    return packages.map(({ name, version, description, time, keywords, dist, homepage, bugs, author, license }, i) => {
       // TODO: move format license to API side.
-      const license = formatLicense(pkg.license);
+      const _license = formatLicense(license);
       return (
         <Fragment key={i}>
           {i !== 0 && <Divider />}
-          <Package {...{ name, dist, version, author, description, license, time, keywords, homepage, bugs }} />
+          <Package
+            {...{ name, dist, version, author, description, license: _license, time, keywords, homepage, bugs }}
+          />
         </Fragment>
       );
     });
   };
 
-  const hasPackages: () => boolean = () => props.packages.length > 0;
+  const hasPackages: () => boolean = () => packages.length > 0;
 
   return (
     <div className={'package-list-items'}>
