68df471bb45edea54b3e77b596789c80d0925b40
Priscila Oliveira <priscilawebdev@gmail.com>
2019-11-10 13:01:13 +0100

    feat: added js mock for static files

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -26,6 +26,6 @@ module.exports = {
     'github-markdown-css': '<rootDir>/node_modules/identity-obj-proxy',
     '\\.(png)$': '<rootDir>/node_modules/identity-obj-proxy',
     '\\.(svg)$': '<rootDir>/jest/unit/empty.ts',
-    '\\.(woff|woff2)$': '<rootDir>/node_modules/identity-obj-proxy',
+    '\\.(woff|woff2)$': '<rootDir>/jest/unit/fileMock.ts',
   },
 };
