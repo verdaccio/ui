836a1e2136c3744b6782300a73a25a8d84809258
Priscila Oliveira <priscilawebdev@gmail.com>
2019-11-11 13:48:01 +0100

    refactor: removed fonts type

--- types/files.d.ts
+++ types/files.d.ts
@@ -3,15 +3,6 @@ declare module '*.svg' {
   export default content;
 }
 
-declare module '*.woff' {
-  const value: string;
-  export = value;
-}
-declare module '*.woff2' {
-  const value: string;
-  export = value;
-}
-
 declare module '*.png' {
   const value: string;
   export = value;
