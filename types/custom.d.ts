09fe1db8508aff5bad702425a874bfc805dfed9e
Priscila Oliveira <priscilawebdev@gmail.com>
2019-11-12 08:18:05 +0100

    Refactor(#240): Created Reset CSS and added local fonts

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,11 +1,3 @@
-// https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.png';
-
 declare module 'localstorage-memory' {
   const memoryStorage: Storage;
   export default memoryStorage;
