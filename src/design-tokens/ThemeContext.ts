675ee980ee2c4c789e52d38553f751bb219d1270
Priscila Oliveira <priscilawebdev@gmail.com>
2020-04-23 08:20:41 +0200

    feat(lng): Added change language on the fly (#456)
    * feat(lng): added change language on the fly
    
    * fixed dropdown
    
    * applied feedbacks
    
    * added translation
    
    * updated bundlesize
    
    * fixed error
    
    * updated snaps
    
    * added french language
    
    * added language in storage
    
    * updated styles
    
    * fixed tests

--- src/design-tokens/ThemeContext.ts
+++ src/design-tokens/ThemeContext.ts
@@ -1,8 +1,11 @@
 import { createContext } from 'react';
 
+import { Language } from '../../i18n/config';
 interface Props {
   isDarkMode: boolean;
   setIsDarkMode: (isDarkMode: boolean) => void;
+  language: Language;
+  setLanguage: (language: Language) => void;
 }
 
 const ThemeContext = createContext<undefined | Props>(undefined);
