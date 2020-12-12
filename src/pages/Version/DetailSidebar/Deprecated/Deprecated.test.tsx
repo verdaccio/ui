cd356a657cdc737aff7194d233386978abed4392
Juan Picado <juanpicado19@gmail.com>
2020-12-12 19:11:04 +0100

    feat: display deprecated packages

---
+++ src/pages/Version/DetailSidebar/Deprecated/Deprecated.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { render, cleanup, screen } from 'verdaccio-ui/utils/test-react-testing-library';
+
+import { DetailContextProvider } from '../../context';
+
+import Deprecated from './Deprecated';
+
+describe('test Deprecated', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  const packageMeta = {
+    latest: {
+      packageName: 'foo',
+      version: '1.0.0',
+      deprecated: 'duuuude, this is deprecated',
+      maintainers: [],
+      contributors: [],
+    },
+  };
+
+  test('should render the deprecated message', () => {
+    render(
+      <DetailContextProvider value={{ packageMeta }}>
+        <Deprecated message={packageMeta.latest.deprecated} />
+      </DetailContextProvider>
+    );
+
+    expect(screen.getByText('duuuude, this is deprecated')).toBeInTheDocument();
+  });
+});
