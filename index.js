537d11f3a81abe18c81f4751026584f7a041e7ee
Juan Picado <juanpicado19@gmail.com>
2021-03-23 14:57:02 +0100

    chore: export default

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ exports.staticPath = path.join(__dirname, 'static');
 exports.manifest = require('./static/manifest.json');
 exports.manifestFiles = {
   js: ['runtime.js', 'vendors.js', 'main.js'],
-  css: [],
   ico: 'favicon.ico',
+};
+
+module.exports = () => {
+  return require('./static/manifest.json');
 };
