e09d6479a3a348f4a84a351d9fe490d68edb86f4
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-08-12 21:14:49 +0200

    fix(ui): Update weird button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import { default as MaterialUIButton, ButtonProps } from '@material-ui/core/Button';
 import React, { forwardRef } from 'react';
 
+// this is a button
 type ButtonRef = HTMLButtonElement;
 
 const Button = forwardRef<ButtonRef, ButtonProps>(function Button(props, ref) {
